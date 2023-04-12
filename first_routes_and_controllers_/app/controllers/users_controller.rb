55487e8fb8fa81f2c0eb5adb4aca78efcee08d3f
Arvid Hossain <arvidh97@gmail.com>
2023-04-12 17:05:57 -0400

    Completed

--- first_routes_and_controllers_/app/controllers/users_controller.rb
+++ first_routes_and_controllers_/app/controllers/users_controller.rb
@@ -12,13 +12,30 @@ class UsersController < ApplicationController
         else
             render json: @user.errors.full_messages, status: 422
         end
-
     end
 
     def show
         @user = User.find(params[:id])
         render json: @user 
     end
+
+    def update
+        @user = User.find(params[:id])
+
+        if @user.update(user_params)
+            redirect_to user_url(@user)
+        else 
+            render json: @user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to users_url
+    end
+
+    private
 
     def user_params
         params.require(:user).permit(:name, :email)
